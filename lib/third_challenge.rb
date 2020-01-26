7c00134e4c057e1459bb59c2c9e5419313a10cbf
Damon R <dmarcr1997@gmail.com>
2020-01-26 20:30:34 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,29 +6,29 @@ def third_challenge
         age: "53"
         },
       :matriarch => {
-        name: "Lady Montague"
+        name: "Lady Montague",
         age: "54"
         },
       :hero => {
-        name: "Romeo"
-        age: "15"
+        name: "Romeo",
+        age: "15",
         status: "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        name: "Lord Capulet"
+        name: "Lord Capulet",
         age: "51"
       },
       :matriarch => {
-        name: "Lady Capulet"
+        name: "Lady Capulet",
         age: "51"
       },
       :heroine => {
-        name: "Juliet"
-        age: "15"
-        :status => "alive"
+        name: "Juliet",
+        age: "15",
+        status: "alive"
       },
       :heroine_friends => []
    }
